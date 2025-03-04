1680660d5fe9a6527760644253fd1b8730dffcd4
Rafael Gonçalves Martins <128759336+DungeonCodes@users.noreply.github.com>
2025-03-03 22:23:25 -0300

    config django

--- sistema_prontuarios/backend/config/settings.py
+++ sistema_prontuarios/backend/config/settings.py
@@ -74,11 +74,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+import os
+import dj_database_url
+from dotenv import load_dotenv
+
+# Carregar variáveis do .env
+load_dotenv()
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+}
+
+
+
 
 
 # Password validation
