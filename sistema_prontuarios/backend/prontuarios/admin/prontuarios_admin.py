5fd8bec8b58e453d69733da60bc02ce4f99a2d7c
gabistorres <gabis.torres@gmail.com>
2025-03-22 23:32:16 -0300

    feat: add Prontuarios model and admin interface, update settings for PostgreSQL

--- sistema_prontuarios/backend/prontuarios/admin/prontuarios_admin.py
+++ sistema_prontuarios/backend/prontuarios/admin/prontuarios_admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Prontuarios  # Importe o modelo
+from prontuarios.models import Prontuarios  # Importe o modelo
 
 @admin.register(Prontuarios)
 class ProntuarioAdmin(admin.ModelAdmin):
