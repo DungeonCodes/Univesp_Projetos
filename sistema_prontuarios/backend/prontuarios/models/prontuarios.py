4bb39440dfe9ad512f0a3a8f2907ab972781d063
gabistorres <gabis.torres@gmail.com>
2025-03-22 23:32:16 -0300

    feat: add Prontuarios model and admin interface, update settings for PostgreSQL

--- sistema_prontuarios/backend/prontuarios/models/prontuarios.py
+++ sistema_prontuarios/backend/prontuarios/models/prontuarios.py
@@ -1,15 +1,14 @@
 from django.db import models
+import uuid # importa a biblioteca uuid
 
 class Prontuarios(models.Model):
+    id_prontuario = models.UUIDField(primary_key=True,editable=False, default=uuid.uuid4, verbose_name="ID Prontuário") # cria um campo id_prontuario do tipo UUIDField
     credential_id = models.BigIntegerField(unique=True,verbose_name="CNS")
     name = models.CharField(max_length=255,verbose_name="Nome")
     last_name = models.CharField(max_length=50,verbose_name="Sobrenome")
     micro_area = models.IntegerField(verbose_name="Microárea")
     family = models.IntegerField(verbose_name="Família")
 
-
-    # ADICIONE ESTE CAMPO PARA FORÇAR UMA MUDANÇA
-    observacoes = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}{self.last_name} Prontuário: {self.family}-{self.micro_area}"
