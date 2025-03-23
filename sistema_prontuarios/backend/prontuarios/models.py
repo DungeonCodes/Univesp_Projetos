bc4510695b4e6c919f8cfa2b4713d829a53515e7
gabistorres <gabis.torres@gmail.com>
2025-03-23 17:20:04 -0300

    feat: sis_user

--- sistema_prontuarios/backend/prontuarios/models.py
+++ sistema_prontuarios/backend/prontuarios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 import uuid # importa a biblioteca uuid
 
 class Prontuarios(models.Model):
@@ -12,4 +13,18 @@ class Prontuarios(models.Model):
 
     def __str__(self):
         return f"{self.name}{self.last_name} Prontuário: {self.family}-{self.micro_area}"
+
+# cria a relaçao de Usuários do sistema, com criptografia hash para senha
+class SisUser(AbstractUser):
+    id_user = models.OneToOneField(Prontuarios, on_delete=models.CASCADE, null=True, blank=True, verbose_name="ID Usuário") 
+    class UserLevel(models.TextChoices):
+        ADMIN_APP = 'admin_app', 'Administrador do Sistema'
+        ADMIN_STAFF = 'admin_staff', 'Administrador Gerencial'
+        ADMIN_SAME = 'admin_same', 'Administrador do Same'
+        USER = 'user', 'Usuário Comum'
+
+    user_level = models.CharField(max_length=50, choices=UserLevel.choices, null=True, default=UserLevel.USER, verbose_name="Nível de Acesso")
     
+  
+    def __str__(self):
+        return f"{self.username} - {self.email} - {self.get_user_level_display()}"
