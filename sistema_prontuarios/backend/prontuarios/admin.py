aa4d0c2effcb2e193226abaa5d5789031cc883df
gabistorres <gabis.torres@gmail.com>
2025-03-25 22:12:58 -0300

    fix: update comments and remove accents in settings, admin, and models

--- sistema_prontuarios/backend/prontuarios/admin.py
+++ sistema_prontuarios/backend/prontuarios/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Prontuarios, SisUser  # Importe o modelo
-# modo de visualização no admin do Django prontuários
+from .models import Prontuarios, SisUser  # Importar o modelo
+
+# Modo de visualizacao no admin do Django para Prontuarios
 @admin.register(Prontuarios)
 class ProntuarioAdmin(admin.ModelAdmin):
     list_display = ('micro_area','family','credential_id', 'name', 'last_name')
     search_fields = ('name', 'credential_id','family','micro_area')
     list_filter = ('family','micro_area')
-# modo de visualização no admin do Django prontuários
+
+# Modo de visualizacao no admin do Django para usuarios
 @admin.register(SisUser)    
 class SisUserAdmin(UserAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name']
