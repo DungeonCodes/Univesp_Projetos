997ff8e33864f301a4379cd0c8829a4c54f3c846
gabistorres <gabis.torres@gmail.com>
2025-04-06 19:28:46 -0300

    feat:profissionals

--- sistema_prontuarios/backend/prontuarios/models.py
+++ sistema_prontuarios/backend/prontuarios/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid  # Importa a biblioteca uuid
 
+class Professional(models.Model):
+    id_professional = models.UUIDField(
+        primary_key=True,
+        editable=False,
+        default=uuid.uuid4,
+        verbose_name="ID Profissional"  # Campo ID sem acento
+    )
+    credential_id = models.BigIntegerField(unique=True, verbose_name="Matricula")
+    name_professional = models.CharField(max_length=255, verbose_name="Nome")
+    last_name_professional = models.CharField(max_length=50, verbose_name="Sobrenome")
+    job_title = models.CharField(max_length=50, verbose_name="Cargo")
+    usage = models.CharField(max_length=50, verbose_name="Permissões")
+
+
+    def __str__(self):
+        return f"{self.name_professional} {self.last_name_professional}"  # Removido acento
+
+
+
 class Prontuarios(models.Model):
     id_prontuario = models.UUIDField(
         primary_key=True,
@@ -12,20 +31,22 @@ class Prontuarios(models.Model):
     credential_id = models.BigIntegerField(unique=True, verbose_name="CNS")
     name = models.CharField(max_length=255, verbose_name="Nome")
     last_name = models.CharField(max_length=50, verbose_name="Sobrenome")
-    micro_area = models.IntegerField(verbose_name="Microarea")  # Removido acento
-    family = models.IntegerField(verbose_name="Familia")        # Removido acento
+    micro_area = models.IntegerField(verbose_name="Microarea")   
+    family = models.IntegerField(verbose_name="Familia")        
 
     def __str__(self):
-        return f"{self.name}{self.last_name} Prontuario: {self.family}-{self.micro_area}"  # Removido acento
+        return f"{self.name}{self.last_name} Prontuario: {self.family}-{self.micro_area}" 
 
 # Cria a relacao de Usuarios do sistema, com criptografia hash para senha
 class SisUser(AbstractUser):
     id_user = models.OneToOneField(
-        Prontuarios,
+        Professional,
+        to_field="credential_id",
+        related_name="Matricula",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
-        verbose_name="ID Usuario"  # Removido acento
+        verbose_name="ID Usuario"  
     )
 
     class UserLevel(models.TextChoices):
@@ -69,14 +90,9 @@ class Team(models.Model):
     id_team = models.IntegerField(unique=True, verbose_name="ID Team")
 
 class Transfer(models.Model):
-    id_transfer = models.BigIntegerField(unique=True, verbose_name="ID Transferencia")
-    id_professional = models.ForeignKey(Professional, on_delete=models.CASCADE)
-    id_receptor = models.BigIntegerField(
-        Professional
-        
-        def __str__(self):
-        return f"{self.id_professional} -{self.name_professional}"
-    )
+    id_transfer = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4, verbose_name="ID Transferencia")  # Campo ID sem acento
+    id_sender = models.ForeignKey(Professional, to_field="credential_id", related_name="sender", verbose_name="ID Administrativo")
+    id_receiver = models.ForeignKey(Professional, to_field="credential_id", related_name="receiver", verbose_name="ID Receptor")
     checkout = models.DateTimeField(
         verbose_name="Data de check-out",
         editable=False
@@ -86,3 +102,6 @@ class Transfer(models.Model):
         editable=False
     )
     cns = models.ForeignKey(Prontuarios, verbose_name="CNS")
+
+    def __str__(self):
+        return f"Saída Prontuário: {Prontuarios.micro_area}.{Prontuarios.family}. - Entrgue por: {Professional.name_professional} - Returado por: {Professional.name_professional}"
